b6ed0ce7b1f65ce01b8e945d22eac71a24fbf5fa
TDeMon <de_monForever@mail.ru>
2014-03-14 13:40:39 +0400

    first version

---
+++ NETDllViewer/AssemblyView.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace NETDllViewer
+{
+    public class AssemblyView : INotifyPropertyChanged
+    {
+        public AssemblyView()
+        {
+            UsingAssembly = Assembly.GetExecutingAssembly();            
+        }
+
+        public static AssemblyView Default
+        {
+            get
+            {
+                return new AssemblyView();
+            }
+        }
+
+        private Assembly _assembly;
+        public Assembly UsingAssembly
+        {
+            get
+            {
+                return _assembly;
+            }
+            set
+            {
+                _assembly = value;                
+                NotifyPropertyChanged("UsingAssembly");
+                Classes = new List<Type>(UsingAssembly.GetTypes());
+            }
+        }
+
+        private List<Type> _classes;
+        public List<Type> Classes
+        {
+            get
+            {
+                /*if (UsingAssembly != null)
+                {
+                    List<Type> lst = new List<Type>(UsingAssembly.GetTypes());
+                    return lst;
+                }
+                return null;*/
+                return _classes;
+            }
+            set
+            {
+                _classes = value;
+                NotifyPropertyChanged("Classes");
+            }
+        }
+
+        public List<Type> Interfaces
+        {
+            get;
+            set;
+        }
+
+        public List<MethodInfo> Methods
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+    }
+
+    [ValueConversion(typeof(Type), typeof(IEnumerable<Type>))]
+    public class ClassToInterfacesConverter : IValueConverter
+    {
+        public static ClassToInterfacesConverter Default
+        {
+            get
+            {
+                return new ClassToInterfacesConverter();
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            string FilterStr = parameter as string;
+            if (string.IsNullOrEmpty(FilterStr))
+            {
+                return ((Type)value).GetInterfaces();
+            }
+            else
+            {
+                return ((Type)value).GetInterfaces().Where(type => type.ToString().Contains(FilterStr));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [ValueConversion(typeof(Type), typeof(IEnumerable<MethodInfo>))]
+    public class ClassToMethodsConverter : IValueConverter
+    {
+        public static ClassToMethodsConverter Default
+        {
+            get
+            {
+                return new ClassToMethodsConverter();
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            return ((Type)value).GetMethods();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ NETDllViewer/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Reflection;
+using Microsoft.Win32;
+
+namespace NETDllViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        Assembly assembly
+        {
+            get
+            {
+                AssemblyView asm = (AssemblyView)FindResource("Viewer");
+                return asm.UsingAssembly;
+            }
+            set
+            {
+                AssemblyView asm = (AssemblyView)FindResource("Viewer");
+                asm.UsingAssembly = value;
+            }
+        }
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Library (.dll) | *.dll|Executable (.exe) | *.exe|All files (*.*) | *.*";
+            if (dialog.ShowDialog()  == true)
+            {
+                assembly = Assembly.LoadFrom(dialog.FileName);                
+            }
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void mainWindow_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                try
+                {
+                    string[] droppedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);          
+                    assembly = Assembly.LoadFrom(droppedFiles[0]);
+                }
+                catch(Exception ex)
+                {
+                    //txtAssemblyName.Text = ex.Message;
+                    assembly = null;
+                }
+            }
+        }
+
+        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        {           
+            assembly = Assembly.GetExecutingAssembly();
+        }
+
+        private void CollectionViewSource_Filter(object sender, FilterEventArgs e)
+        {
+            string filterStr = txtFilter.Text;
+            if (string.IsNullOrEmpty(filterStr))
+            {
+                e.Accepted = true;
+                return;
+            }
+            filterStr = filterStr.ToLowerInvariant();
+            if (e.Item.ToString().ToLowerInvariant().Contains(filterStr))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource collectionMethods = TryFindResource("collectionMethods") as CollectionViewSource;
+            CollectionViewSource collectionInterfaces = TryFindResource("collectionInterfaces") as CollectionViewSource;
+            if (collectionMethods != null)
+            {
+                collectionMethods.View.Refresh();
+            }
+            if (collectionInterfaces != null)
+            {
+                collectionInterfaces.View.Refresh();
+            }
+        }
+    }
+}
